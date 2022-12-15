024304e181fce79bf8017d5adaa37ea88d3fb39a
PilyBernal <pilybernal@gmail.com>
2022-12-15 14:07:17 -0600

    Permite visualizar las notas en formato de grilla

--- src/components/auth/AllNotes.js
+++ src/components/auth/AllNotes.js
@@ -24,47 +24,13 @@ const AllNotes = () => {
   }, [])
 
   return (
-    <div>
+    <div className='allNotes'>
       {allNotes.map((nota) =>
         OneNote(nota)
       )}
     </div>
   )
 
-}
-/*const AllNotes = () => {
-  const [notes, setNotes] = useState([])
-  const querySnapshot = getDocs(collection(db, "notitas"))
-  //console.log(querySnapshot)
-  querySnapshot.then(resultado => setNotes(resultado.map(res => res.data())))  //resultado.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    //console.log(doc.id, " => ", doc.data());
-    
-  return (
-    <div className="listOfNotes">HOLA
-      {notes.map(note => {
-
-        <OneNote
-          key={note.id}
-          title={note.title}
-          content={note.content}
-        />
-
-      })}
-    </div>
-)}*/
-//} 
-
-//console.log(querySnapshot)
-
-/*const listOfNotes = useState.map((notes) =>
-  <li key={notes.id}>
-    {notes.text}
-  </li>
-);
-console.log(listOfNotes)
- 
-}))*/
-//OneNote() 
+} 
 
 export default AllNotes;
